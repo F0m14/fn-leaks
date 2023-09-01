2394df33797b196b7d732178059024155917a431
- X D - <81095246+F0m14@users.noreply.github.com>
2023-09-01 15:50:36 +0300

    Add files via upload

--- leaks.js
+++ leaks.js
@@ -3,9 +3,16 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     const leaks = [
       {
-        title: "No Leaks",
-        content: "The Leaks Will Coming Soon",
+        image: 'anime1.jpg',
+        title: "Collaboration",
+        content: "Maybe theres an upcoming collaboration.",
         date: "2023-09-01",
+      },
+      {
+        image: 'anime2.jpg',
+      },
+      {
+        image: 'anime3.jpg',
       },
     ];
 
@@ -25,13 +32,15 @@ window.addEventListener('DOMContentLoaded', (event) => {
         const contentElement = document.createElement('p');
         contentElement.textContent = leak.content;
 
-        const dateElement = document.createElement('p');
-        dateElement.classList.add('date');
-        dateElement.textContent = "Published In: " + leak.date;
+        if (leak.date) {
+          const dateElement = document.createElement('p');
+          dateElement.classList.add('date');
+          dateElement.textContent = "Published In: " + leak.date;
+          leakElement.appendChild(dateElement);
+        }
 
         leakElement.appendChild(titleElement);
         leakElement.appendChild(contentElement);
-        leakElement.appendChild(dateElement);
 
         leaksContainer.appendChild(leakElement);
     });
